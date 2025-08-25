c2075a56c9ca057647ba91a0b63a01f0a8775750
smolnik42 <kalyan.smol@mail.ru>
2025-08-25 15:57:13 +0300

    Autotest1

---
+++ main.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=100)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://google.com/")
+
+    search_box = page.locator('[name="q"]')
+    search_box.click()
+    search_box.fill('sql')
+    search_box.press('Enter')
+
+    page.wait_for_selector('#search', timeout=10000)
+
+    context.close()
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
